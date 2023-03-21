dfee175ae5b82f7f1758742b42450f38067067af
afifwahyu03 <afifwahyu@students.amikom.ac.id>
2023-03-21 12:57:22 +0700

    praktikum1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Taxi
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Riyanto";
+            taxi.onDuty = false; taxi.onDuty = true;
+            taxi.numPassanger = 0881221345;
+
+
+            taxi.info();
+            taxi.naik();
+            taxi.turun();
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
---
+++ Taxi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public Boolean onDuty { get; set; }
+        public int numPassanger { get; set; }
+
+        public void info()
+        {
+            Console.WriteLine("\n Taxi Info \n");
+            Console.WriteLine(" Nama Driver : {0}", DriverName);
+
+            if (onDuty == true)
+            {
+                Console.WriteLine(" Kerja : Iya", onDuty);
+            }
+            else
+            {
+                Console.WriteLine(" Kerja : Tidak", onDuty);
+            }
+
+            Console.WriteLine(" Nomor Pelanggan : {0}", numPassanger);
+
+        }
+
+        public void naik()
+        {
+            Console.WriteLine("\n {0} Sedang Mengantarkan Penumpang", DriverName);
+        }
+
+        public void turun()
+        {
+            Console.WriteLine("\n {0} Sedang Menurunkan Peumpang", DriverName);
+        }
+    }
+}
